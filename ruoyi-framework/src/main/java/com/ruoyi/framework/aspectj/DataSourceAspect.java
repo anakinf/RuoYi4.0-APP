94e59f72de992c8b5484ed9d03ee00aa7edf8427
zhangmrit <zhangmrit@126.com>
2018-11-16 10:53:52 +0800

    集成通用mapper,修改多数据源切面，增加彩色日志，修改多环境处理

--- ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/DataSourceAspect.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/DataSourceAspect.java
@@ -1,23 +1,20 @@
 package com.ruoyi.framework.aspectj;
 
-import java.lang.reflect.Method;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import com.ruoyi.common.annotation.DataSource;
-import com.ruoyi.common.utils.StringUtils;
+
+import com.ruoyi.common.enums.DataSourceType;
 import com.ruoyi.framework.datasource.DynamicDataSourceContextHolder;
+
 
 /**
  * 多数据源处理
- * 
- * @author ruoyi
  */
 @Aspect
 @Order(1)
@@ -26,26 +23,21 @@ public class DataSourceAspect
 {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Pointcut("@annotation(com.ruoyi.common.annotation.DataSource)")
-    public void dsPointCut()
-    {
-
-    }
-
-    @Around("dsPointCut()")
+    @Around("execution(* com.ruoyi..*ServiceImpl.*(..))")
     public Object around(ProceedingJoinPoint point) throws Throwable
     {
-        MethodSignature signature = (MethodSignature) point.getSignature();
-
-        Method method = signature.getMethod();
-
-        DataSource dataSource = method.getAnnotation(DataSource.class);
-
-        if (StringUtils.isNotNull(dataSource))
+        // 获取到当前执行的方法名
+        String methodName = point.getSignature().getName();
+        if (isSlave(methodName))
         {
-            DynamicDataSourceContextHolder.setDateSoureType(dataSource.value().name());
+            // 标记为读库,可以自定义选择数据源
+            DynamicDataSourceContextHolder.setDateSoureType(DataSourceType.SLAVE.name());
         }
-
+        else
+        {
+            // 标记为写库
+            DynamicDataSourceContextHolder.setDateSoureType(DataSourceType.MASTER.name());
+        }
         try
         {
             return point.proceed();
@@ -55,5 +47,17 @@ public class DataSourceAspect
             // 销毁数据源 在执行方法之后
             DynamicDataSourceContextHolder.clearDateSoureType();
         }
+    }
+
+    /**
+     * 判断是否为读库
+     *
+     * @param methodName
+     * @return
+     */
+    private boolean isSlave(String methodName)
+    {
+        // 方法名以query、find、get开头的方法名走从库
+        return StringUtils.startsWithAny(methodName, new String[]{"query", "find", "get", "select"});
     }
 }
