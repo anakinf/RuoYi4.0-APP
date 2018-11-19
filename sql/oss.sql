445c418752b2697213b8780e27a4dbb5a220e9c6
zhangmrit <zhangmrit@126.com>
2018-11-19 13:26:54 +0800

    乱码

--- sql/oss.sql
+++ sql/oss.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `sys_oss` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `file_name` varchar(64) NOT NULL DEFAULT '' COMMENT '�ļ���',
-  `file_suffix` varchar(10) NOT NULL DEFAULT '' COMMENT '�ļ���׺��',
-  `url` varchar(200) NOT NULL COMMENT 'URL��ַ',
-  `create_time` datetime DEFAULT NULL COMMENT '����ʱ��',
-  `create_by` varchar(64) NOT NULL DEFAULT '' COMMENT '�ϴ���',
-  `service` tinyint(2) NOT NULL DEFAULT '1' COMMENT '������',
+  `file_name` varchar(64) NOT NULL DEFAULT '' COMMENT '文件名',
+  `file_suffix` varchar(10) NOT NULL DEFAULT '' COMMENT '文件后缀名',
+  `url` varchar(200) NOT NULL COMMENT 'URL地址',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_by` varchar(64) NOT NULL DEFAULT '' COMMENT '上传人',
+  `service` tinyint(2) NOT NULL DEFAULT '1' COMMENT '服务商',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='�ļ��ϴ�';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='文件上传';
 
 INSERT INTO `ry`.`sys_config` (`config_id`, `config_name`, `config_key`, `config_value`, `config_type`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES ('3', 'oss�洢����', 'sys.oss.cloudStorage', '{\"aliyunAccessKeyId\":\"\",\"aliyunAccessKeySecret\":\"\",\"aliyunBucketName\":\"\",\"aliyunDomain\":\"\",\"aliyunEndPoint\":\"\",\"aliyunPrefix\":\"\",\"qcloudBucketName\":\"\",\"qcloudDomain\":\"\",\"qcloudPrefix\":\"\",\"qcloudSecretId\":\"\",\"qcloudSecretKey\":\"\",\"qiniuAccessKey\":\"\",\"qiniuBucketName\":\"ios-app\",\"qiniuDomain\":\"\",\"qiniuPrefix\":\"upload\",\"qiniuSecretKey\":\"\",\"type\":1}', 'Y', 'admin', '2018-03-16 11:33:00', '', '2018-03-16 11:33:00', 'oss云存储配置');
 INSERT INTO `ry`.`sys_menu` (`menu_id`, `menu_name`, `parent_id`, `order_num`, `url`, `menu_type`, `visible`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES ('1056', '文件管理', '1', '10', '/system/oss', 'C', '0', 'system:oss:view', '#', 'admin', '2018-11-16 13:59:45', '', NULL, '');
