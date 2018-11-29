59cff4410d186a1929338b6ef9fd2077b0c80036
zhangmrit <zhangmrit@126.com>
2018-11-29 13:20:13 +0800

    修改oss模块报错信息提示

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/exception/user/OssException.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/exception/user/OssException.java
@@ -1,18 +1,16 @@
 package com.ruoyi.framework.web.exception.user;
 
-import com.ruoyi.framework.web.exception.base.BaseException;
-
 /**
  * OSS信息异常类
  * 
- * @author ruoyi
+ * @author zmr
  */
-public class OssException extends BaseException
+public class OssException extends RuntimeException
 {
     private static final long serialVersionUID = 1L;
 
     public OssException(String msg)
     {
-        super("oss", new Object[]{msg});
+        super(msg);
     }
 }
