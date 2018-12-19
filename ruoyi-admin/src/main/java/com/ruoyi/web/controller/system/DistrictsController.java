f75fc94feb1567da9970894eb720ed85a677039f
zhangmrit <zhangmrit@126.com>
2018-12-19 13:53:37 +0800

    地区

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/DistrictsController.java
@@ -0,0 +1,129 @@
+package com.ruoyi.web.controller.system;
+
+import java.util.List;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.base.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.page.TableDataInfo;
+import com.ruoyi.common.utils.ExcelUtil;
+import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.system.domain.Districts;
+import com.ruoyi.system.service.IDistrictsService;
+
+/**
+ * 地区 信息操作处理
+ * 
+ * @author ruoyi
+ * @date 2018-12-19
+ */
+@Controller
+@RequestMapping("/system/districts")
+public class DistrictsController extends BaseController
+{
+    private String prefix = "system/districts";
+	
+	@Autowired
+	private IDistrictsService districtsService;
+	
+	@RequiresPermissions("system:districts:view")
+	@GetMapping()
+	public String districts()
+	{
+	    return prefix + "/districts";
+	}
+	
+	/**
+	 * 查询地区列表
+	 */
+	@RequiresPermissions("system:districts:list")
+	@PostMapping("/list")
+	@ResponseBody
+	public TableDataInfo list(Districts districts)
+	{
+		startPage();
+        List<Districts> list = districtsService.selectDistrictsList(districts);
+		return getDataTable(list);
+	}
+	
+	
+	/**
+	 * 导出地区列表
+	 */
+	@RequiresPermissions("system:districts:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(Districts districts)
+    {
+    	List<Districts> list = districtsService.selectDistrictsList(districts);
+        ExcelUtil<Districts> util = new ExcelUtil<Districts>(Districts.class);
+        return util.exportExcel(list, "districts");
+    }
+	
+	/**
+	 * 新增地区
+	 */
+	@GetMapping("/add")
+	public String add()
+	{
+	    return prefix + "/add";
+	}
+	
+	/**
+	 * 新增保存地区
+	 */
+	@RequiresPermissions("system:districts:add")
+	@Log(title = "地区", businessType = BusinessType.INSERT)
+	@PostMapping("/add")
+	@ResponseBody
+	public AjaxResult addSave(Districts districts)
+	{		
+		return toAjax(districtsService.insertDistricts(districts));
+	}
+
+	/**
+	 * 修改地区
+	 */
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, ModelMap mmap)
+	{
+		Districts districts = districtsService.selectDistrictsById(id);
+		mmap.put("districts", districts);
+	    return prefix + "/edit";
+	}
+	
+	/**
+	 * 修改保存地区
+	 */
+	@RequiresPermissions("system:districts:edit")
+	@Log(title = "地区", businessType = BusinessType.UPDATE)
+	@PostMapping("/edit")
+	@ResponseBody
+	public AjaxResult editSave(Districts districts)
+	{		
+		return toAjax(districtsService.updateDistricts(districts));
+	}
+	
+	/**
+	 * 删除地区
+	 */
+	@RequiresPermissions("system:districts:remove")
+	@Log(title = "地区", businessType = BusinessType.DELETE)
+	@PostMapping( "/remove")
+	@ResponseBody
+	public AjaxResult remove(String ids)
+	{		
+		return toAjax(districtsService.deleteDistrictsByIds(ids));
+	}
+	
+}
