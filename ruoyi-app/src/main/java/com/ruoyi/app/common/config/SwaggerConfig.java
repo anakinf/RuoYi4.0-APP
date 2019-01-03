4456fa9acf2715346ffda12a0191cbd8fc50700b
zhangmrit <zhangmrit@126.com>
2019-01-03 16:11:53 +0800

    swagger

--- ruoyi-app/src/main/java/com/ruoyi/app/common/config/SwaggerConfig.java
+++ ruoyi-app/src/main/java/com/ruoyi/app/common/config/SwaggerConfig.java
@@ -1,21 +1,24 @@
 package com.ruoyi.app.common.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
 import com.ruoyi.common.config.Global;
 
 import io.swagger.annotations.ApiOperation;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -34,14 +37,35 @@ public class SwaggerConfig
     @Bean
     public Docket createRestApi()
     {
-        return new Docket(DocumentationType.SWAGGER_2)
-                // 详细定制
-                .apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-                // 指定当前包路径
-                // .apis(RequestHandlerSelectors.basePackage("com.ruoyi.web.controller.tool"))
-                // .apis(basePackage("com.ruoyi.web.controller.system,com.ruoyi.web.controller.tool"))
-                // 扫描所有 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).forCodeGeneration(true).select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .apis(RequestHandlerSelectors.any()).paths(PathSelectors.regex("^.*(?<!error)$")).build()
+                .securitySchemes(securitySchemes()).securityContexts(securityContexts());
+    }
+
+    private List<ApiKey> securitySchemes()
+    {
+        List<ApiKey> apiKeyList = new ArrayList<ApiKey>();
+        apiKeyList.add(new ApiKey("token", "token", "header"));
+        return apiKeyList;
+    }
+
+    private List<SecurityContext> securityContexts()
+    {
+        List<SecurityContext> securityContexts = new ArrayList<>();
+        securityContexts.add(SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("^(?!auth).*$")).build());
+        return securityContexts;
+    }
+
+    List<SecurityReference> defaultAuth()
+    {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference("token", authorizationScopes));
+        return securityReferences;
     }
 
     /**
@@ -52,60 +76,5 @@ public class SwaggerConfig
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder().title("标题：若依管理系统_接口文档").description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
                 .contact(new Contact(Global.getName(), null, null)).version("版本号:" + Global.getVersion()).build();
-    }
-
-    /**
-     * Predicate that matches RequestHandler with given base package name for the class of the handler method.
-     * This predicate includes all request handlers matching the provided basePackage
-     *
-     * @param basePackage - base package of the classes
-     * @return this
-     */
-    public static Predicate<RequestHandler> basePackage(final String basePackage)
-    {
-        return new Predicate<RequestHandler>()
-        {
-            @Override
-            public boolean apply(RequestHandler input)
-            {
-                return declaringClass(input).transform(handlerPackage(basePackage)).or(true);
-            }
-        };
-    }
-
-    /**
-     * 处理包路径配置规则,支持多路径扫描匹配以逗号隔开
-     * 
-     * @param basePackage 扫描包路径
-     * @return Function
-     */
-    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage)
-    {
-        return new Function<Class<?>, Boolean>()
-        {
-            @Override
-            public Boolean apply(Class<?> input)
-            {
-                for (String strPackage : basePackage.split(","))
-                {
-                    boolean isMatch = input.getPackage().getName().startsWith(strPackage);
-                    if (isMatch)
-                    {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        };
-    }
-
-    /**
-     * @param input RequestHandler
-     * @return Optional
-     */
-    @SuppressWarnings("deprecation")
-    private static Optional<? extends Class<?>> declaringClass(RequestHandler input)
-    {
-        return Optional.fromNullable(input.declaringClass());
     }
 }
