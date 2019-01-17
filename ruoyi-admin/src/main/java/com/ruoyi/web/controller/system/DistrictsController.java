03b350381460c919839c2126ee0d21f1fb90502c
zhangmrit <zhangmrit@126.com>
2019-01-17 10:59:08 +0800

    spring配置驼峰敏感异常

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/DistrictsController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/DistrictsController.java
@@ -3,6 +3,7 @@ package com.ruoyi.web.controller.system;
 import java.util.Date;
 import java.util.List;
 
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,7 +18,6 @@ import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.base.AjaxResult;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.page.TableDataInfo;
-import com.ruoyi.common.utils.ExcelUtil;
 import com.ruoyi.framework.util.ShiroUtils;
 import com.ruoyi.framework.web.base.BaseController;
 import com.ruoyi.system.domain.Districts;
